40e283e6efaddf031f6d5e0fef0d59fc6b3eb0b1
Rob <49131756+robscallsign@users.noreply.github.com>
2019-10-22 10:19:33 -0400

    add example of private api keys file

---
+++ private_api_keys.lua
@@ -0,0 +1,17 @@
+
+-- HypeMan BotID and Channel private API key files
+-- Right now the discord bot uses two bots - one for general takeoff/landing/flight hours/deaths/ejections
+-- and another bot/channel only for the LSO carrier landing grades
+
+PRIVATE_HYPEMAN_BOT_CLIENT_ID = 'Bot NTIzOTE3NjQ5MjQ0MTkyNzc2.Dv3hMw.zep917Di1DqFq4j7IJPxKzdTOLA'
+PRIVATE_HYPEMAN_CHANNEL_ID = '575724528378970112' -- #music
+
+-- Carrier Grade BotID and Channel
+PRIVATE_CQ_CLIENT_ID = 'Bot NjAzMzk5MTkxNDU2MjUxOTI4.XTe80Q._Ccsy9qmnytCAZuDXq-kBuGoLhE' -- tony
+PRIVATE_CQ_CHANNEL_ID = '603412609072496670' -- #snafu
+
+-- Private Command ID, allow someone to send !commands to HypeMan through discord private message
+-- currently uses !connect and !disconnect for hypeman_voice to connect to a Discord voice channel
+PRIVATE_COMMAND_ID = '361935537571102720'
+
+SERVERNAME = 'Rob Rulez!!11!'
