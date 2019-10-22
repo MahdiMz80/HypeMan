1286290453611764e17becefdb3509c2c3b51a04
Rob <49131756+robscallsign@users.noreply.github.com>
2019-10-22 08:01:54 -0700

    Update private_api_keys.lua

--- private_api_keys.lua
+++ private_api_keys.lua
@@ -3,11 +3,11 @@
 -- Right now the discord bot uses two bots - one for general takeoff/landing/flight hours/deaths/ejections
 -- and another bot/channel only for the LSO carrier landing grades
 
-PRIVATE_HYPEMAN_BOT_CLIENT_ID = 'Bot NTIzOTE3NjQ5MjQ0MTkyNzc2.Dv3hMw.zep917Di1DqFq4j7IJPxKzdTOLA'
+PRIVATE_HYPEMAN_BOT_CLIENT_ID = 'Bot private.bot.id.goes.here'
 PRIVATE_HYPEMAN_CHANNEL_ID = '575724528378970112' -- #music
 
 -- Carrier Grade BotID and Channel
-PRIVATE_CQ_CLIENT_ID = 'Bot NjAzMzk5MTkxNDU2MjUxOTI4.XTe80Q._Ccsy9qmnytCAZuDXq-kBuGoLhE' -- tony
+PRIVATE_CQ_CLIENT_ID = 'Bot private.bot.id.goes.here'
 PRIVATE_CQ_CHANNEL_ID = '603412609072496670' -- #snafu
 
 -- Private Command ID, allow someone to send !commands to HypeMan through discord private message
