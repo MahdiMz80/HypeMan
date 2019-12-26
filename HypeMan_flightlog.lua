2f4d810a16a68c525676251a9435b0f6dc750b7a
Rob <49131756+robscallsign@users.noreply.github.com>
2019-12-26 14:14:23 -0500

    Trying to fix the logic error with clearing submitted flight logs

--- HypeMan_flightlog.lua
+++ HypeMan_flightlog.lua
@@ -33,30 +33,23 @@ HypeMan.UDPSendSocket = socket.udp()
 HypeMan.UDPSendSocket:settimeout(0)
 
 -- Table to store takeoff times
-HypeManTakeOffTime = {}
+local HypeManTakeOffTime = {}
 
 -- Table to store the Flight Log information
-HypeManFlightLog = {}
-
-HypeManRefuelingTable = {}
-
--- table to store hits on aircraft
-HypeManHitTable = {}
+local HypeManFlightLog = {}
 
 -- This is the function that can be called in any mission to send a message to discord.
 -- From the Mission Editor add a Trigger and a DO SCRIPT action and enter as the script:
 -- HypeMan.sendBotMessage('Roses are Red.  Violets are blue.  I only wrote this poem to test my Discord Bot.')
 
 HypeMan.sendBotTable = function(tbl)
-	-- env.info(msg)  -- for debugging
 	local tbl_json_txt = JSON:encode(tbl)
 	socket.try(HypeMan.UDPSendSocket:sendto(tbl_json_txt, '127.0.0.1', 10081))
-	-- msg = nil  -- setting to nil in attempt to debug why message queue seems to grow
 end
 
 HypeMan.sendDebugMessage = function(msg)
 	if HypeManDebugMessages then
-		messageTable = {}
+		local messageTable = {}
 		messageTable.messageType = 1
 		messageTable.messageString = "DEBUG_MESSAGE: " .. msg
 		HypeMan.sendBotTable(messageTable)
@@ -65,7 +58,7 @@ HypeMan.sendDebugMessage = function(msg)
 end
 
 HypeMan.sendBotMessage  = function(msg)
-	messageTable = {}
+	local messageTable = {}
 	messageTable.messageType = 1
 	messageTable.messageString = msg
 	HypeMan.sendBotTable(messageTable)
@@ -90,10 +83,10 @@ local function addFlightTime(flID)
 	
 	if HypeManFlightLog[flID].departureTimer ~= nil then
 		HypeManFlightLog[flID].trackedTime = HypeManFlightLog[flID].trackedTime +  timer.getAbsTime() - HypeManFlightLog[flID].departureTimer
-		HypeManFlightLog[flID].departureTimer = nil -- set to nil to let other event handlers know that the time has been tracked
+		-- set to nil so that other events can call addFlightTime but the time won't get added
+		HypeManFlightLog[flID].departureTimer = nil 
 	end
 end
-
 
 local function sendFlightLog(flID)
 	
@@ -109,14 +102,15 @@ local function sendFlightLog(flID)
 		resetAfterSubmission(flID)
 	else
 		-- I don't think this code will ever get called now.  Once the flight log is submitted, that unit ID gets reset in the table now
-		HypeMan.sendDebugMessage(' sendFlightLog() called log for ID '.. ' but it was already submitted.  Disregard.')	
+		HypeMan.sendDebugMessage(' went to sendFlightLog() for ID '.. ' but it was already submitted.')	
 	end
 end
+
 
 local function resetAfterSubmission(flID)
 -- This function resets some of the tracked flight statistics after a flight log is submitted.  This allows handling the case
 -- when someone lands, and then takes off again after a few minutes on the ground or on the carrier.
-
+	HypeMan.sendDebugMessage('begin resetAfterSubmission for ID: ' .. flID)
 	--HypeManFlightLog[flID].pending = false
 	HypeManFlightLog[flID].trackedTime = 0
 	HypeManFlightLog[flID].numTakeoffs = 0
@@ -142,12 +136,13 @@ local function resetAfterSubmission(flID)
 	
 	-- airStart can be reset.  The only way that it can be called again is with another BIRTH event
 	HypeManFlightLog[flID].airStart = 0
-	-- elem.theatre = env.mission.theatre
+	
+	HypeMan.sendDebugMessage('end of resetAfterSubmission for ID: ' .. flID)
 end
 
 local function flightLogNewEntry()
 -- this function fills in an empty flight log entry with no details about the flight
-	elem = {}
+	local elem = {}
 	elem.messageType = 3 -- messageType = 3 for flight logging
 
 	elem.submitted = false
@@ -183,7 +178,7 @@ end
 local function FlightLogCreateNewEntry(flID, flType, flAirStart, flCallsign, flCoalition)
 	-- this is a new entry to the flight log
 	
-	logEntry = flightLogNewEntry()
+	local logEntry = flightLogNewEntry()
 
 	-- convert the bool airstart flag to a number for adding to the stats table
 	local airStartNum = 0
@@ -206,7 +201,7 @@ end
 local function FlightLogDeparture(flID, flAirfield)
 	HypeMan.sendDebugMessage(' Flight Log Departure called.  ID: ' .. flID .. ' airfield: ' .. flAirfield)
 	if HypeManFlightLog[flID] == nil then
-		HypeMan.sendDebugMessage(' object ID was nil...')
+		HypeMan.sendDebugMessage(' FlightLogDeparture object ID was nil...')
 		return
 	end
 	
@@ -220,7 +215,7 @@ local function FlightLogDeparture(flID, flAirfield)
 	HypeManFlightLog[flID].pending = false
 	
 	-- the departure handler here is the only place where a flight log can be opened by changing a submitted flight log back to false
-	HypeManFlightLog[flID].submitted = false  
+	HypeManFlightLog[flID].submitted = false	
 	HypeManFlightLog[flID].numTakeoffs = HypeManFlightLog[flID].numTakeoffs + 1
 	HypeManFlightLog[flID].departureTimer = timer.getAbsTime()
 end
@@ -245,10 +240,8 @@ local function FlightLogArrival(flID, flAirfield)
 
 	HypeManFlightLog[flID].numLandings = HypeManFlightLog[flID].numLandings + 1
 
-	-- Every landing prepare to send the flight log by starting a callback timer that sends the flight log a certain time
-	-- in the future
-	-- the other ways that the flight log will be sent:
-	-- dead, eject, crash
+	-- Landing triggers a flight log submission in HypeManFlightLogTimer seconds in the future
+	-- the other ways that the flight log will be sent: dead, eject, crash
 	HypeManFlightLog[flID].pending = true
 	timer.scheduleFunction(sendFlightLog, flID, timer.getTime() + HypeManFlightLogTimer )
 end
@@ -258,7 +251,7 @@ local function HypeManGetName(initiator)
 		return false, nil;
 	end
 
-	-- need to be careful here because it seems like the player has a chance to die before we can query their name
+	-- need to be careful here because it seems like the player has a chance to die before we can query their name it seems
 	local statusflag, name = pcall(Unit.getPlayerName, initiator)
 
 	if statusflag == false then
@@ -304,9 +297,10 @@ local function HypeManTakeOffHandler(event)
 
 		if HypeManFlightLogging then
 			if HypeManFlightLog[flID] == nil then
-				-- If the object to be tracked doesn't exist yet then create a new entry for it
-				HypeMan.sendDebugMessage('FlightLogCreateEntry called inside S_EVENT_TAKEOFF called for object ID: ' .. flID)				
-				FlightLogCreateNewEntry(flID, acType, event.initiator:inAir(), name, event.initiator:getCoalition())
+				-- If the object to be tracked doesn't exist yet then create a new entry for it.
+				-- TODO: I'm not sure this code is ever called				
+				--HypeMan.sendBotMessage('ROB - FlightLogCreateEntry called inside S_EVENT_TAKEOFF.  You were not sure if this would ever be called... well, it was.')
+				FlightLogCreateNewEntry(flID, acType, 0, name, event.initiator:getCoalition())
 			end
 			
 			HypeMan.sendDebugMessage('FlightLogDeparture called for object ID: ' .. flID)
@@ -345,13 +339,16 @@ local function HypeManLandingHandler(event)
 			airfieldName = 'Unknown'
 		end
 
-		-- TODO - not sure this second check is necessary, or ever called.
-		if airfieldName == nil then
+		if airfieldName == nil or airfieldName == '' then
 			airfieldName = 'Unknown'
 		end
 
 		local flID = Unit.getID(event.initiator)
 
+		if flID == nil then
+			return
+		end
+		
 		if HypeManFlightLogging then
 			if HypeManFlightLog[flID] == nil then
 				-- this check is here because AI units don't trigger an birth event, but then will trigger a landing event
@@ -367,7 +364,7 @@ local function HypeManLandingHandler(event)
 		local t = HypeManTakeOffTime[flID]
 		
 		if t == nil then
-			-- AI airstart unit does not trigger a b event
+			-- AI airstart unit does not trigger a birth event
 			return
 		end
 
@@ -588,7 +585,7 @@ end
 local function HypeManRefuelingHandler(event)
 	if event.id == world.event.S_EVENT_REFUELING then
 		local statusflag, name = HypeManGetName(event.initiator)
-
+		
 		if statusflag == false then
 			return
 		end
@@ -601,6 +598,8 @@ local function HypeManRefuelingHandler(event)
 			return
 		end
 
+		HypeMan.sendBotMessage('ROB - refueling event handler was called in multiplayer.')
+		
 		local flID = Unit.getID(event.initiator)
 		
 		if HypeManFlightLogging and HypeManFlightLog[flID] ~= nil then
